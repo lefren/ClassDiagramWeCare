73709e9ea625e3054c2926df5db77b007766748d
DESKTOP-TL9629T\bcamaster <ichsan.ilyasa@binus.ac.id>
2023-03-28 13:06:49 +0700

    woi ini yang baru (contoh)

--- WeCare/MainFrame.java
+++ WeCare/MainFrame.java
@@ -3,5 +3,7 @@ package WeCare;
 public class MainFrame {
     public static void main(String[] args) {
         System.out.println("Tes");
+        System.out.println("Tes2");
+        System.out.println("Tes3");
     }
 }
