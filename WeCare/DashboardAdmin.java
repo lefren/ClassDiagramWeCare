b43be8806e9899580a708272d83b58f35e0cd9a5
DESKTOP-TL9629T\bcamaster <ichsan.ilyasa@binus.ac.id>
2023-04-12 09:26:40 +0700

    no message

--- WeCare/DashboardAdmin.java
+++ WeCare/DashboardAdmin.java
@@ -1,4 +1,7 @@
 package OOP.JFRAME;
 
 public class DashboardAdmin {
+    public static void main(String[] args) {
+        System.out.println("hELLO WORLD");
+    }
 }
--- WeCare/DashboardUser.java
+++ WeCare/DashboardUser.java
@@ -1,4 +1,7 @@
 package OOP.JFRAME;
 
 public class DashboardUser {
+    public static void main(String[] args) {
+        System.out.println("hELLO WORLD");
+    }
 }
