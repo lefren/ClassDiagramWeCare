967032fd60f0ebe5fb016bd06d7864426f07da44
DESKTOP-TL9629T\bcamaster <ichsan.ilyasa@binus.ac.id>
2023-05-03 16:31:05 +0700

    update 1.0.30

--- WeCare/controller/Payment2.java
+++ WeCare/controller/Payment2.java
@@ -28,10 +28,9 @@ public class Payment2 {
     private ActionEvent event;
     @FXML
     void statusbtnclick(ActionEvent event) throws IOException {
-        int appointmentid = 3;
         String newStatus = "success";
 
-        Appointment updatedAppointment = updateAppointmentStatus(appointmentid, newStatus);
+        Appointment updatedAppointment = updateAppointmentStatus(newStatus);
 
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../view/dashboardpatientappointment.fxml")));
         Scene scene = new Scene(root);
@@ -42,28 +41,63 @@ public class Payment2 {
         stage.centerOnScreen();
     }
 
-    private Appointment updateAppointmentStatus(int id, String status) {
+//    private Appointment updateAppointmentStatus(int id, String status) {
+//        try {
+//            Connection conn = DriverManager.getConnection(DB_URL, USERNAME, PASS);
+//
+//            String query = "SELECT id, hospital, doctor, status FROM appointment";
+//            PreparedStatement preparedStatement = conn.prepareStatement(query);
+//            ResultSet resultSet = preparedStatement.executeQuery();
+//
+//
+//            String sql = "UPDATE appointment SET status = ? WHERE id = ?";
+//            PreparedStatement updatestmt = conn.prepareStatement(sql);
+//            updatestmt.setString(1, status);
+//            updatestmt.setInt(2, id);
+//
+//            int updatedRows = updatestmt.executeUpdate();
+//
+//            if (updatedRows > 0) {
+//                return new Appointment(id, null,null, status, null);
+//            }
+//
+//            updatestmt.close();
+//            conn.close();
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//        return null;
+//    }
+
+    private Appointment updateAppointmentStatus(String status) {
+        int appointmentid = 0;
         try {
             Connection conn = DriverManager.getConnection(DB_URL, USERNAME, PASS);
 
-            String sql = "UPDATE appointment SET status = ? WHERE id = ?";
-            PreparedStatement updatestmt = conn.prepareStatement(sql);
-            updatestmt.setString(1, status);
-            updatestmt.setInt(2, id);
-
-            int updatedRows = updatestmt.executeUpdate();
-
-            if (updatedRows > 0) {
-                return new Appointment(id, null,null, status, null);
+            String sqlQuery = "SELECT id FROM appointment";
+            PreparedStatement queryStmt = conn.prepareStatement(sqlQuery);
+            ResultSet rs = queryStmt.executeQuery();
+            while (rs.next()) {
+                appointmentid = rs.getInt(1);
             }
 
-            updatestmt.close();
+            String sqlUpdate = "UPDATE appointment SET status = ? WHERE id = ?";
+            PreparedStatement updateStmt = conn.prepareStatement(sqlUpdate);
+            updateStmt.setString(1, status);
+            updateStmt.setInt(2, appointmentid);
+            int updatedRows = updateStmt.executeUpdate();
+            updateStmt.close();
+
+            if (updatedRows > 0) {
+                return new Appointment(appointmentid, null,null, status, null);
+            }
+
+            rs.close();
+            queryStmt.close();
             conn.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return null;
     }
-
-
 }
